c50f1f5d52aa4e5d7b72e0dd62722868a5a1c4c1
Darksaint-iCode <ibroyusufola@gmail.com>
2020-04-05 13:41:39 -0700

    Accept Number: Give words of number complete!

--- src/number/to/words/NumberToWords.java
+++ src/number/to/words/NumberToWords.java
@@ -22,7 +22,7 @@ public class NumberToWords {
         "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Hundreed", "Thousand", "Million"};
     
     static Scanner inputNum = new Scanner(System.in);
-    static int dNum, length;
+    static int dNum, length, tempC, digitC, countedD;
     
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
@@ -30,11 +30,54 @@ public class NumberToWords {
         System.out.print("Input number you want to convert: ");
         dNum = inputNum.nextInt();
         checkLength();
+        checkDigit();
     }
 
     private static void checkLength() {
         length = String.valueOf(dNum).length();
         System.out.println("\nLength of your Number is: " + length);
+        System.out.println("");
+    }
+
+    private static void checkDigit() {
+        //making a copy of the input number
+        tempC = dNum;
+        
+        //counting digits in the input number
+        while(dNum > 0)
+        {
+            dNum = dNum / 10;
+            countedD++;
+        }
+        while(tempC > 0)
+        {
+            digitC = tempC % 10;
+            if(digitC == 0){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[0]);  
+            }else if(digitC == 1){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[1]);  
+            }else if(digitC == 2){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[2]);  
+            }else if(digitC == 3){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[3]);  
+            }else if(digitC == 4){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[4]);  
+            }else if(digitC == 5){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[5]);  
+            }else if(digitC == 6){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[6]);  
+            }else if(digitC == 7){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[7]);  
+            }else if(digitC == 8){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[8]);  
+            }else if(digitC == 9){
+                System.out.println("Digit at place "+countedD+" is: "+valueIn[9]);  
+            }else{
+                System.out.println("Digit at place " + countedD + " is not recognised by Machine :(");     
+            }
+            tempC = tempC / 10;
+            countedD--;
+        }
     }
     
 }
